3276f44d6ee819c87f80c9fd8be7f62306075b8a
Sergey Boronin <bo-ronin@yandex.ru>
2021-09-20 16:00:52 +0300

    markup: main&cart finished

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ function App() {
     <div className="page flex flex-col w-10/12 items-center mx-auto bg-white rounded-xl mt-10 relative">
       <div className="cart-modal fixed w-full h-full z-10 left-0 top-0">
         <div className="cart-content w-1/4 min-h-full fixed right-0 p-7 bg-white flex flex-col">
-          <h2 className="text-2xl font-bold mb-4">Корзина</h2>
-          <div className="cart flex-auto">
-
+          <h2 className="text-2xl font-bold mb-6">Корзина</h2>
+          <button className="cart-close-btn absolute top-9 right-12 cursor-pointer">
+            <img src="/images/cart-close-btn.svg" alt="Закрыть корзину" />
+          </button>
+          <div className="cart flex-auto flex flex-col mb-6">
             <div className="cart-item rounded-3xl p-6 shadow relative flex justify-between items-center">
               <img
                 className="cart-image w-1/4"
@@ -19,11 +21,10 @@ function App() {
                 <div className="flex flex-col mb-2 text-sm">
                   <span className="font-bold">12 999 руб.</span>
                 </div>
-
               </div>
-              <button className="">
-                  <img src="/images/close-button-img.svg" alt="" />
-                </button>
+              <button className="cart-remove-btn">
+                <img src="/images/close-button-img.svg" alt="" />
+              </button>
             </div>
             <div className="cart-item rounded-3xl p-6 shadow relative flex justify-between items-center">
               <img
@@ -38,11 +39,10 @@ function App() {
                 <div className="flex flex-col mb-2 text-sm">
                   <span className="font-bold">12 999 руб.</span>
                 </div>
-
               </div>
-              <button className="">
-                  <img src="/images/close-button-img.svg" alt="" />
-                </button>
+              <button className="cart-remove-btn">
+                <img src="/images/close-button-img.svg" alt="" />
+              </button>
             </div>
             <div className="cart-item rounded-3xl p-6 shadow relative flex justify-between items-center">
               <img
@@ -57,28 +57,88 @@ function App() {
                 <div className="flex flex-col mb-2 text-sm">
                   <span className="font-bold">12 999 руб.</span>
                 </div>
-
               </div>
-              <button className="">
-                  <img src="/images/close-button-img.svg" alt="" />
-                </button>
+              <button className="cart-remove-btn">
+                <img src="/images/close-button-img.svg" alt="" />
+              </button>
             </div>
-
-
+            <div className="cart-item rounded-3xl p-6 shadow relative flex justify-between items-center">
+              <img
+                className="cart-image w-1/4"
+                src="/images/sneaker.png"
+                alt="sneaker"
+              />
+              <div className="flex flex-col justify-between w-1/2">
+                <p className="text-sm mb-2">
+                  Мужские Кроссовки Nike Blazer Mid Suede
+                </p>
+                <div className="flex flex-col mb-2 text-sm">
+                  <span className="font-bold">12 999 руб.</span>
+                </div>
+              </div>
+              <button className="cart-remove-btn">
+                <img src="/images/close-button-img.svg" alt="" />
+              </button>
+            </div>
+            <div className="cart-item rounded-3xl p-6 shadow relative flex justify-between items-center">
+              <img
+                className="cart-image w-1/4"
+                src="/images/sneaker.png"
+                alt="sneaker"
+              />
+              <div className="flex flex-col justify-between w-1/2">
+                <p className="text-sm mb-2">
+                  Мужские Кроссовки Nike Blazer Mid Suede
+                </p>
+                <div className="flex flex-col mb-2 text-sm">
+                  <span className="font-bold">12 999 руб.</span>
+                </div>
+              </div>
+              <button className="cart-remove-btn">
+                <img src="/images/close-button-img.svg" alt="" />
+              </button>
+            </div>
+            <div className="cart-item rounded-3xl p-6 shadow relative flex justify-between items-center">
+              <img
+                className="cart-image w-1/4"
+                src="/images/sneaker.png"
+                alt="sneaker"
+              />
+              <div className="flex flex-col justify-between w-1/2">
+                <p className="text-sm mb-2">
+                  Мужские Кроссовки Nike Blazer Mid Suede
+                </p>
+                <div className="flex flex-col mb-2 text-sm">
+                  <span className="font-bold">12 999 руб.</span>
+                </div>
+              </div>
+              <button className="cart-remove-btn">
+                <img src="/images/close-button-img.svg" alt="" />
+              </button>
+            </div>
           </div>
           <div className="cart-footer">
-              <div className="cart-summary flex justify-between items-baseline">
-                <span className="pr-1">Итого:</span>
-                <span className="cart-border-block"></span>
-                <span className="pl-1 font-bold">12 999 руб.</span>
-              </div>
-              <div className="cart-tax flex justify-between items-baseline mb-6">
+            <div className="cart-summary flex justify-between items-baseline">
+              <span className="pr-1">Итого:</span>
+              <span className="cart-border-block"></span>
+              <span className="pl-1 font-bold">12 999 руб.</span>
+            </div>
+            <div className="cart-tax flex justify-between items-baseline mb-6">
               <span className="pr-1">Налог 5%:</span>
               <span className="cart-border-block"></span>
-                <span className="pl-1 font-bold">12 999 руб.</span>
-                </div>
-                <button className="w-full bg-green-300 h-12 text-lg font-bold rounded-xl">Оформить заказ</button>
+              <span className="pl-1 font-bold">12 999 руб.</span>
             </div>
+            <button className="cart-order-button w-full h-12  rounded-xl flex justify-center items-center pl-12">
+              <span className="cart-order-button-text text-lg font-bold mr-8 text-white">
+                Оформить заказ
+              </span>
+              <img
+                className="cart-order-button-arrow"
+                src="/images/cart-button-arrow.svg"
+                alt=""
+              />
+            </button>
+          </div>
         </div>
       </div>
 
@@ -101,7 +161,7 @@ function App() {
               </div>
             </div>
             <div className="user flex items-center">
-              <div className="cart flex mr-7">
+              <div className="header-cart flex mr-7">
                 <img
                   src="/images/cart.svg"
                   alt=""
