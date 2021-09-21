e43380daa552d1c145a248197bfbfaed56715275
Sergey Boronin <bo-ronin@yandex.ru>
2021-09-21 21:57:18 +0300

    init cards added

---
+++ src/components/initialCards.js
@@ -0,0 +1,13 @@
+const initData = [
+    {"name": "Мужские Кроссовки Nike Blazer Mid Suede", "price": "10000"},
+    {"name": "Мужские Кроссовки Nike Air Max 270", "price": "11000"},
+    {"name": "Кроссовки Puma X Aka Boku Future Rider", "price": "12000"},
+    {"name": "Мужские Кроссовки Nike Kyrie 7", "price": "13000"},
+    {"name": "Мужские Кроссовки Jordan Air Jordan 11", "price": "13000"},
+    {"name": "Мужские Кроссовки Nike LeBron XVIII", "price": "14000"},
+    {"name": "Мужские Кроссовки Nike Lebron XVIII Low", "price": "15000"},
+    {"name": "Кроссовки Puma X Aka Boku Future Rider", "price": "16000"},
+    {"name": "Мужские Кроссовки Nike Kyrie Flytrap IV", "price": "17000"},
+] 
+
+export default initData;
